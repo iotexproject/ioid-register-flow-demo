d5ee601e9574d47405217b5aa4d535b927149443
DLHTX <949729789@qq.com>
2024-08-20 15:35:25 +0800

    refactor: rename device to DepinDevice and clean up unused DIDKit code

--- examples/with-contract-flow/device.ts
+++ examples/with-contract-flow/device.ts
@@ -1,9 +1,9 @@
 import { ethers } from "ethers";
 import { VERIFY_PROXY_ADDRESS } from ".";
-import DIDKit from '@spruceid/didkit';
+// import DIDKit from '@spruceid/didkit';
 
 export class Device {
-  name = "WatchX"
+  name = "DepinDevice"
   device = ethers.Wallet.createRandom()
   get address() {
     return this.device.address
@@ -14,26 +14,24 @@ export class Device {
   }
 
   get diddoc() {
-    //todo: generate diddoc from sdk
-    const key = DIDKit.generateEd25519Key();
-    // There are two helpful functions to obtain a DID and the `did:key`
-    // `verificationMethod` from the key.
-    const did = DIDKit.keyToDID('key', key);
-    const verificationMethod = DIDKit.keyToVerificationMethod('key', key);
-    const vc = DIDKit.issueCredential({
-      "@context": "https://www.w3.org/2018/credentials/v1",
-      "id": "http://example.org/credentials/3731",
-      "type": ["VerifiableCredential"],
-      "issuer": did,
-      "issuanceDate": "2020-08-19T21:41:50Z",
-      "credentialSubject": {
-        "id": "did:example:d23dd687a7dc6787646f2eb98d0"
-      }
-    }, {
-      "proofPurpose": "assertionMethod",
-      "verificationMethod": verificationMethod
-    }, key);
-    return vc
+    return {}
+    // const key = DIDKit.generateEd25519Key();
+    // const did = DIDKit.keyToDID('key', key);
+    // const verificationMethod = DIDKit.keyToVerificationMethod('key', key);
+    // const vc = DIDKit.issueCredential({
+    //   "@context": "https://www.w3.org/2018/credentials/v1",
+    //   "id": "http://example.org/credentials/3731",
+    //   "type": ["VerifiableCredential"],
+    //   "issuer": did,
+    //   "issuanceDate": "2020-08-19T21:41:50Z",
+    //   "credentialSubject": {
+    //     "id": "did:example:d23dd687a7dc6787646f2eb98d0"
+    //   }
+    // }, {
+    //   "proofPurpose": "assertionMethod",
+    //   "verificationMethod": verificationMethod
+    // }, key);
+    // return vc
   }
 
   async sign() {
